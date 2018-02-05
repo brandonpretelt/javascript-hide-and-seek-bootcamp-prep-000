8845d69dda2ee1169f1a81adcf02697657416301
Brandon Pretelt <brandonpretelt93@gmail.com>
2018-02-05 11:16:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,5 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
-for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (i + n).toString()
-}
+
 }
