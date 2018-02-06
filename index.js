28650e898da2dbd39c8f6c8cf887fcaad97f5244
Brandon Pretelt <brandonpretelt93@gmail.com>
2018-02-06 10:25:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function deepestChild() {
         if (divs[i].innerHTML === "") {
             return false;
         } else {
-           return divs[i].querySelector(":last-child").innerHTML;
+           return divs[i].querySelector(":last-child");
         }
     }
 }
