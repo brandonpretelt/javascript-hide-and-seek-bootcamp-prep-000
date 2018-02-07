ae42b50cb34c55a107cc6e340ffaca307402f032
Brandon Pretelt <brandonpretelt93@gmail.com>
2018-02-07 08:19:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function increaseRankBy(n) {
 function deepestChild() {
     let divs = document.getElementById("grand-node").querySelectorAll("div");
     let tree;
-    for (let i = 0; i < divs.length-1; i +=1 ) {
-      tree = divs[i].querySelector("div");
+    for (let i = 0; i < divs.length; i +=1 ) {
+      tree = divs[i].querySelector(":last-child");
     }
     return tree;
 }
